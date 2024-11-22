eda549e1f9228b8b4f3b5213e8365eaeb4c49946
realeoncito <joleon248@unicah.edu>
2024-11-22 00:01:10 -0600

    Avance testimonios.

--- src/components/testimonios/LargeCards.jsx
+++ src/components/testimonios/LargeCards.jsx
@@ -10,7 +10,16 @@ const LargeCards = ()=>{
                     <div className="CardContent bgGreen">
                         <h2>Carlos Slim</h2>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laborum quaerat facere ex. Consequuntur earum culpa quod quo eum numquam sequi, quam officia eligendi officiis?
+                        Probar estos deliciosos platillos es de las mejores cosas de visitar la ciudad de los pinares.
+                        </p>
+                    </div>
+                </div>
+                <div className="LargeCard">
+                    <img src="https://picsum.photos/id/75/1024/720" alt="imagen de la carta"/>
+                    <div className="CardContent bgWhite">
+                        <h2>Cooperativa Cacil</h2>
+                        <p>
+                        Realizar el brunch anual de iniciación en BenditoVerde fue de las mejores decisiones que pudimos tomar
                         </p>
                     </div>
                 </div>
--- src/pages/Testimonios.jsx
+++ src/pages/Testimonios.jsx
@@ -1,10 +1,15 @@
 import HeroTestimonios from "../components/testimonios/HeroTestimonios"
 import LargeCards from "../components/testimonios/LargeCards"
+import SmallCards from "../components/testimonios/SmallCards"
+import { ContextProvider } from "../Context/testimonioscontext"
 
 const Testimonios = () => {
-    return<>
-        <HeroTestimonios/>
-        <LargeCards/>
+    return <>
+        <ContextProvider>
+            <HeroTestimonios />
+            <LargeCards />
+            <SmallCards />
+        </ContextProvider>
     </>
 }
 
