0f04375117df1d381d684ff5809e3a22448175e4
dry <karurosu.vihir@gmail.com>
2024-11-26 10:14:59 -0600

    solo cambie import "./cardBase.css" a import "./cardBase"

--- src/components/producto/Modal.jsx
+++ src/components/producto/Modal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./cardBase.css";
+import "./cardBase";
 
 const Modal = ({ items, setmodal }) => {
   const [showDetails, setShowDetails] = useState(false); 
