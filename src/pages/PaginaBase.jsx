15463749145ac3c60becea16c6eac14f4dbe7d48
Erick112358 <erickandreirias@gmail.com>
2024-11-29 22:02:23 -0600

    Animacion de carga

--- src/pages/PaginaBase.jsx
+++ src/pages/PaginaBase.jsx
@@ -3,6 +3,7 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import styled from "styled-components"
 import { ContextProvider } from "../Context/globalcontext"
+import Loader from "../components/loader"
 
 const Main = styled.main`
     @media (max-width: 760px) {
@@ -18,6 +19,7 @@ const PaginaBase = () => {
                 <Outlet />
             </Main>
             <Footer/>
+            <Loader/>
         </ContextProvider>
     </>
 }
